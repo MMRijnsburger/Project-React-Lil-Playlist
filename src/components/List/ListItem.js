f3548599dc9c40dab83894a4a4c6ecce47e6417f
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 18:20:34 +0200

    add song props to list item

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const ListItem = () => {
+const ListItem = (props) => {
     return (
-        <li>list item</li>
+        <li>
+            <span> {props.item.title} </span>
+            <span> {props.item.artist} </span>
+            <span> {props.item.genre} </span>
+            <span> {props.item.rating} </span>
+        </li>
     )
 }
 
