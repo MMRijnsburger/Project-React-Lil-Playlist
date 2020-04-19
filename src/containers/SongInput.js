5f4b0a41047e042d724da77d29bf27329390f129
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 20:59:38 +0200

    add props to InputField and handle the changes

--- src/containers/SongInput.js
+++ src/containers/SongInput.js
@@ -5,14 +5,49 @@ import InputSelect from '../components/InputSelect';
 class SongInput extends React.Component {
     constructor() {
         super()
-        this.state = {}
+        this.state = {
+            title: '',
+            artist: '',
+            genre: '',
+            rating: 0
+        }
+        this.handleTitleChange = this.handleTitleChange.bind(this)
+        this.handleArtistChange = this.handleArtistChange.bind(this)
+    }
+
+    handleTitleChange(title) {
+        // console.log('SI title:', title)
+        this.setState(() => {
+            return {
+                title: title
+            }
+        })
+    }
+
+    handleArtistChange(artist) {
+        // console.log('SI artist:', artist)
+        this.setState(() => {
+            return {
+                artist: artist
+            }
+        })
     }
 
     render() {
         return (
             <form>
-                <InputField id="input-title" />
-                <InputField id="input-artist" />
+                <InputField
+                    id="input-title"
+                    value={this.state.title}
+                    placeholder="voer titel in"
+                    onChange={this.handleTitleChange}
+                />
+                <InputField
+                    id="input-artist"
+                    value={this.state.artist}
+                    placeholder="voer artiest in"
+                    onChange={this.handleArtistChange}
+                />
                 <InputSelect id="input-genre" />
                 <InputSelect id="input-rating" />
                 <button>Voeg toe</button>
