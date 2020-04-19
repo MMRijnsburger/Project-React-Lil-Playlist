760fd7247607edd980f69db951f0c6bba1fca60c
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 18:04:32 +0200

    forward list prop to List component as items prop

--- src/containers/SongList.js
+++ src/containers/SongList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import List from '../components/List/List';
 
-const SongList = () => {
+const SongList = (props) => {
     return (
         <div>
             Songlist
@@ -11,7 +11,7 @@ const SongList = () => {
                 <li>Genre</li>
                 <li>Rating</li>
             </ul>
-            <List />
+            <List items={props.list} />
         </div>
     )
 }
