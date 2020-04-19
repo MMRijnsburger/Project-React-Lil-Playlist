6c2c25671684e65257ed7a198608222c4e2cc0bd
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 18:06:10 +0200

    add map items prop to ListItem components

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,11 +3,19 @@ import ListItem from './ListItem';
 
 class List extends React.Component {
     render() {
+        const items = this.props.items
+        const listItems = items.map(item => (
+            <ListItem
+                key={item.id}
+                item={item}
+            />
+        ))
+
         return (
             <div>
                 List
                 <ul>
-                    <ListItem />
+                    {listItems}
                 </ul>
             </div>
         )
