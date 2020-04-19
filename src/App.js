771dc9d242c12c95f2e7f9aad084866757bf0f19
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 16:26:56 +0200

    add container component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Container from './Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <h1>Header</h1>
       </header>
+      <main>
+        <Container />
+      </main>
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Container extends React.Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>Container</div>
+        )
+    }
+
+}
+
+export default Container
