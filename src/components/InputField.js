fb86739b398da3a376b17d64274e3e69e251b5fb
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 19:29:43 +0200

    change InputField class to function

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,16 +1,9 @@
 import React from 'react';
 
-class InputField extends React.Component {
-    constructor() {
-        super()
-        this.state = {}
-    }
-
-    render() {
-        return (
-            <div>input field</div>
-        )
-    }
+const InputField = () => {
+    return (
+        <div>input field</div>
+    )
 }
 
 export default InputField;
