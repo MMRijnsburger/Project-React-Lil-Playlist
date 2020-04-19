a43d33ec8e3681b625609bb0d01aaa23669ccbee
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-19 22:08:33 +0200

    add GenreData and RatingData

---
+++ src/data/GenreData.js
@@ -0,0 +1,29 @@
+const GenreData = [
+    {
+        id: 1,
+        name: 'Pop',
+        value: 'pop'
+    },
+    {
+        id: 2,
+        name: 'Rock',
+        value: 'rock'
+    },
+    {
+        id: 3,
+        name: 'Soul',
+        value: 'soul'
+    },
+    {
+        id: 4,
+        name: 'Country',
+        value: 'country'
+    },
+    {
+        id: 5,
+        name: 'Hiphop',
+        value: 'hiphop'
+    }
+]
+
+export default GenreData;
