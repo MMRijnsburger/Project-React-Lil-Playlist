ea2e7f408d48c0f22832e5b054c8534c0372044c
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-20 13:24:29 +0200

    change header title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Header</h1>
+        <h1>Lil' Playlist</h1>
       </header>
       <main>
         <Container />
