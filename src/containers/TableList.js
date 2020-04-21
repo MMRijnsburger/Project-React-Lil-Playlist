de4f8b52653163d6ee770669337798fc5d0e14c6
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-21 11:18:21 +0200

    add filters

--- src/containers/TableList.js
+++ src/containers/TableList.js
@@ -8,12 +8,11 @@ const TableList = (props) => {
                 <tr className="song-header">
                     <th className="song-row_item">Song</th>
                     <th className="song-row_item">Artist</th>
-                    <th className="song-row_item">Genre</th>
-                    <th className="song-row_item">Rating</th>
+                    <th className="song-row_item">Genre <select defaultValue="default"><option value="default" disabled hidden>Filter...</option></select></th>
+                    <th className="song-row_item">Rating <select defaultValue="default"><option value="default" disabled hidden>Filter...</option></select></th>
                 </tr>
             </thead>
-
-            <Table items={props.list} onClick={props.onClick}/>
+            <Table items={props.list} onClick={props.onClick} />
         </table>
     )
 }
