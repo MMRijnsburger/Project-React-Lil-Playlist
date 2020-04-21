7d9f78d4e8780a4276b0c7ccdcf627b16e92f1bf
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-04-21 16:00:14 +0200

    add InputSelect component and filter functions

--- src/containers/TableList.js
+++ src/containers/TableList.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import Table from '../components/Table/Table';
+import InputSelect from '../components/InputSelect';
+import GenreData from '../data/GenreData';
+import RatingData from '../data/RatingData';
 
 const TableList = (props) => {
+    const handleGenreChange = (genre) => { console.log('click genre', genre) }
+    const handleRatingChange = (rating) => { console.log('click rating', rating) }
     return (
         <table>
             <thead>
                 <tr className="song-header">
                     <th className="song-row_item">Song</th>
                     <th className="song-row_item">Artist</th>
-                    <th className="song-row_item">Genre <select defaultValue="default"><option value="default" disabled hidden>Filter...</option></select></th>
-                    <th className="song-row_item">Rating <select defaultValue="default"><option value="default" disabled hidden>Filter...</option></select></th>
+                    <th className="song-row_item">Genre
+                    <InputSelect
+                            options={GenreData}
+                            onChange={handleGenreChange}
+                        />
+                    </th>
+                    <th className="song-row_item">Rating
+                    <InputSelect
+                            options={RatingData}
+                            onChange={handleRatingChange}
+                        />
+                    </th>
                 </tr>
             </thead>
             <Table items={props.list} onClick={props.onClick} />
+
         </table>
     )
 }
