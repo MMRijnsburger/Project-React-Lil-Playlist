1cf9c74e905abc1077f84f50f4ac11b2e9b27baa
Michelle Rijnsburger <michelle.rijnsburger@gmail.com>
2020-05-13 14:40:28 +0200

    add delete and filter

--- src/Container.js
+++ src/Container.js
@@ -11,12 +11,15 @@ class Container extends React.Component {
             songList: SongData
         }
         this.handleSongInput = this.handleSongInput.bind(this)
+        this.handleSongDelete = this.handleSongDelete.bind(this)
+        this.handleFilterGenre = this.handleFilterGenre.bind(this)
+        this.handleFilterRating = this.handleFilterRating.bind(this)
     }
 
     handleSongInput(song) {
-        // console.log(song)
-        this.setState(prevState => {
-            const songList = [...prevState.songList]
+        console.log(song)
+        this.setState(state => {
+            const songList = [...state.songList]
             songList.push({
                 id: songList.length + 1,
                 title: song.title,
@@ -32,12 +35,57 @@ class Container extends React.Component {
 
     }
 
+    handleSongDelete(id) {
+        console.log(id)
+        this.setState(state => {
+            const songId = parseInt(id)
+            const songlist = state.songList.filter(item => {
+                return item.id !== songId
+            })
+            return {
+                songList: songlist
+            }
+        })
+    }
+
+    handleFilterGenre(genre) {
+        console.log(`handleFilterGenre(): genre=${genre}`)
+
+        this.setState(state => {
+            const songlist = state.songList.filter(item => {
+                return item.genre === genre
+            })
+            return {
+                songList: songlist
+            }
+        })
+    }
+
+    handleFilterRating(rating) {
+        console.log(`handleFilterRating(): rating=${rating}`)
+
+        this.setState(state => {
+            const songRating = parseInt(rating)
+            const songlist = state.songList.filter(item => {
+                return item.rating === songRating
+            })
+            return {
+                songList: songlist
+            }
+        })
+    }
+
     render() {
         return (
             <div>
                 <SongInput onSubmit={this.handleSongInput} />
                 {/* <SongList list={this.state.songList} /> */}
-                <TableList list={this.state.songList} />
+                <TableList
+                    list={this.state.songList}
+                    onSongDelete={this.handleSongDelete}
+                    onFilterGenre={this.handleFilterGenre}
+                    onFilterRating={this.handleFilterRating}
+                />
             </div>
         )
     }
